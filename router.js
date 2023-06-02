d30f606ea2ba4235c681414df8df436c96176d34
Matias-pixel <marchantmatias706@gmail.com>
2023-06-01 23:56:27 -0400

    router

--- router.js
+++ router.js
@@ -3,9 +3,11 @@ const conexion = require('./database/bd');
 const router = express.Router();
 
 router.get('/',  (req, res)=>{
-
     res.render('login');
+})
 
+router.get('/registro', (req, res)=>{
+    res.render('registro')
 })
 
 // const crud = require('./controllers/crud');
