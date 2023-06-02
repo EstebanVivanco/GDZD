3eb405ce04a4a5401ce279ad1cd76b446fa1d2a6
Matias-pixel <marchantmatias706@gmail.com>
2023-06-02 01:11:23 -0400

    Multer

--- router.js
+++ router.js
@@ -6,11 +6,21 @@ router.get('/',  (req, res)=>{
     res.render('login');
 })
 
-router.get('/registro', (req, res)=>{
-    res.render('registro')
-})
+router.get('/registro', (req, res) => {
+    conexion.query('SELECT * FROM sector;', (error, results2) => {
+        conexion.query('SELECT * FROM tipo_tiendas;', (error, results) => {
+            if (error) {
+                throw error;
+            } else {
+                res.render('registro', { results: results, results2: results2 });
+            }
+        });
+    });
+});
 
 const crud = require('./controllers/crud');
+
 // router.post('/', crud.validacion);
+router.post('/savestore',crud.savestore);
 
 module.exports = router;
